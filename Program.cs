263291bcc479e637ae0856b0e023a411556143ea
Sotnik0xFF <sotnikoff1987@gmail.com>
2023-02-05 11:04:50 +0500

    Update

--- Program.cs
+++ Program.cs
@@ -1,38 +1,74 @@
 ﻿using System;
 using System.Text;
+using System.Globalization;
 
 namespace Module3.ConsoleApp
 {
     internal class Program
     {
+        private const string DateFormat = "dd.MM.yyyy";
+        private const string ExDateFormat = "dd, MMMM, yyyy   ddd";
+
         static void Main()
         {
             Console.OutputEncoding = Encoding.UTF8;
-            Console.InputEncoding = Encoding.Unicode;
+            //Console.InputEncoding = Encoding.Unicode;
 
+            string name = GetName();
+            int age = GetAge();
+            WriteMessage($"Ваше имя {name} и возраст {age}.");
+
+            DateTime birthdate = GetBirthdate();
+            WriteMessage(string.Format("Ваша дата рождения: {0}", birthdate.ToString(ExDateFormat)));
+            Console.ReadKey();
+        }
+
+        static string GetName()
+        {
             Console.Write("Введите имя: ");
-            string name = Console.ReadLine();
+            return Console.ReadLine();
+        }
 
+        static int GetAge()
+        {
             int age;
-            bool isParsed;
+            bool isValid;
             do
             {
                 Console.Write("Введите ваш возраст: ");
+                isValid = int.TryParse(Console.ReadLine(), out age) && age >= 0;
 
-                isParsed = int.TryParse(Console.ReadLine(), out age);
-                if (!isParsed)
+                if (!isValid)
                 {
-                    Console.WriteLine("Ошибка ввода! Нажмите любую клавишу и попробуйте еще раз...");
+                    Console.WriteLine("Неверно указан возраст! Нажмите любую клавишу и попробуйте еще раз...");
                     Console.ReadKey(true);
                 }
-            } while (!isParsed);
+            } while (!isValid);
+            return age;
+        }
 
-            Console.WriteLine($"Ваше имя {name} и возраст {age}.");
+        static DateTime GetBirthdate()
+        {
+            DateTime result;
+            bool isValid;
+            do
+            {
+                Console.Write($"Введите вашу дату рождения ( {DateFormat} ): ");
+                isValid = DateTime.TryParseExact(Console.ReadLine(), DateFormat, null, DateTimeStyles.None, out result);
+                if (!isValid)
+                {
+                    Console.WriteLine("Неверно указана дата рождения! Нажмите любую клавишу и попробуйте еще раз...");
+                    Console.ReadKey(true);
+                }
+            } while (!isValid);
+            return result;
+        }
 
-            Console.Write("Введите вашу дату рождения: ");
-            string birthdate = Console.ReadLine();
-            Console.WriteLine("Ваша дата рождения " + birthdate);
-            Console.ReadKey();
+        static void WriteMessage(string message)
+        {
+            Console.WriteLine("\n********************");
+            Console.WriteLine(message);
+            Console.WriteLine("********************\n\n");
         }
     }
 }
