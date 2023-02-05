8bd1b75ffce59a57111ce8f0c31aaca18de5c37e
Sotnik0xFF <sotnikoff1987@gmail.com>
2023-02-05 06:33:22 +0500

    v1.0

--- Program.cs
+++ Program.cs
@@ -7,7 +7,32 @@ namespace Module3.ConsoleApp
     {
         static void Main()
         {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.InputEncoding = Encoding.Unicode;
 
+            Console.Write("Введите имя: ");
+            string name = Console.ReadLine();
+
+            int age;
+            bool isParsed;
+            do
+            {
+                Console.Write("Введите ваш возраст: ");
+
+                isParsed = int.TryParse(Console.ReadLine(), out age);
+                if (!isParsed)
+                {
+                    Console.WriteLine("Ошибка ввода! Нажмите любую клавишу и попробуйте еще раз...");
+                    Console.ReadKey(true);
+                }
+            } while (!isParsed);
+
+            Console.WriteLine($"Ваше имя {name} и возраст {age}.");
+
+            Console.Write("Введите вашу дату рождения: ");
+            string birthdate = Console.ReadLine();
+            Console.WriteLine("Ваша дата рождения " + birthdate);
+            Console.ReadKey();
         }
     }
 }
